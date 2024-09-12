63a7a04ae8b0da8b89d021e263fc523907d156c5
alemogsten <alexander.mogavero@outlook.com>
2024-09-12 08:25:57 +0200

    Flutter project create

---
+++ .dart_tool/flutter_build/00a8878f08f6c907f0732dfe4f6abbdd/native_assets.d
@@ -0,0 +1,1 @@
+ E:\\Elwodd\\Work\\Flutter\\socks_proxy\\.dart_tool\\flutter_build\\00a8878f08f6c907f0732dfe4f6abbdd\\native_assets.yaml: 
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+
+  @override
+  void dispose() {
+    _primarySocket?.close();
+    _secondarySocket?.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Socket Proxy Example'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: _startProxyServer,
+          child: Text('Start Proxy Server'),
+        ),
+      ),
+    );
+  }
+}
